7a344616efae35a212a1c69a7d907572315de4c9
JasonBorrelli <borrellijason@gmail.com>
2026-09-08 23:28:23 +0200

    feat: implement background color change delay using setTimeout functionality

---
+++ app.js
@@ -0,0 +1,24 @@
+/* 1.mostra un alert 3 secondi dopo il caricamento della pagina
+ */
+
+
+/* setTimeout(() => {
+    alert("ciao world");
+}, 3000);
+ */
+
+/* 2.l click su un elemento, cambia il colore dello sfondo del body dopo un ritardo di 3 secondi */
+
+
+/* 
+const colorBtn = document.querySelector(".colorBtn");
+
+colorBtn.addEventListener("click", function () {
+
+    setTimeout(function () {
+        document.body.style.backgroundColor = "Red";
+    }, 3000); // 3000 millisecondi = 3 secondi
+
+});
+
+ */
